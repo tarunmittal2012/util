e14ac82dbc80189dcf9c401e56ebb77f537015a8
Tarun Mittal <30505734+tarunmittal2012@users.noreply.github.com>
2019-02-27 23:56:01 +0530

    Add files via upload

---
+++ MainActivity.cs
@@ -0,0 +1,41 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Support.Design.Widget;
+using Android.Support.V4.View;
+using Android.Support.V7.App;
+using Android.Widget;
+using Utility.Resources.Adapter;
+
+namespace Utility
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/MyTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        TabLayout mTabLayout;
+        ViewPager viewPager;
+        Toolbar toolbar;
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            SetContentView(Resource.Layout.activity_main);
+            mTabLayout = FindViewById<TabLayout>(Resource.Id.tabLayout);
+            viewPager = FindViewById<ViewPager>(Resource.Id.viewpager);
+//toolbar = FindViewById<Toolbar>(Resource.Id.toolbar);
+  //          toolbar.SetTitle(Resource.String.title);
+
+            mTabLayout.AddTab(mTabLayout.NewTab().SetText(Resource.String.tab1));
+            mTabLayout.AddTab(mTabLayout.NewTab().SetText(Resource.String.tab2));
+            mTabLayout.AddTab(mTabLayout.NewTab().SetText(Resource.String.tab3));
+            FragmentAdapter fragmentAdapter = new FragmentAdapter(SupportFragmentManager, mTabLayout.TabCount);
+            viewPager.Adapter = fragmentAdapter;
+            viewPager.AddOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(mTabLayout));
+            mTabLayout.TabSelected += MTabLayout_TabSelected; 
+        }
+
+        private void MTabLayout_TabSelected(object sender, TabLayout.TabSelectedEventArgs e)
+        {
+            var tab = e.Tab;
+            viewPager.SetCurrentItem(mTabLayout.SelectedTabPosition,true);
+        }
+    }
+}
---
+++ WelcomeFragment.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Support.V4.App;
+using Android.Util;
+using Android.Views;
+using Android.Widget;
+
+namespace Utility
+{
+    public class WelcomeFragment : Fragment
+    {
+        public override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            
+            // Create your fragment here
+        }
+
+        public override View OnCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+        {
+            // Use this to return your custom view for this Fragment
+            // return inflater.Inflate(Resource.Layout.YourFragment, container, false);
+
+            var view = inflater.Inflate(Resource.Layout.fragment_welcome, container, false);
+
+            return view;
+        }
+    }
+    public class ManageFragment : Fragment
+    {
+        public override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Create your fragment here
+        }
+
+        public override View OnCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+        {
+            // Use this to return your custom view for this Fragment
+            // return inflater.Inflate(Resource.Layout.YourFragment, container, false);
+
+            var view = inflater.Inflate(Resource.Layout.fragment_manage, container, false);
+
+            return view;
+        }
+    }
+    public class ShipFragment : Fragment
+    {
+        public override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Create your fragment here
+        }
+
+        public override View OnCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+        {
+            // Use this to return your custom view for this Fragment
+            // return inflater.Inflate(Resource.Layout.YourFragment, container, false);
+            var view = inflater.Inflate(Resource.Layout.fragment_ship, container, false);
+
+            return view;
+        }
+    }
+}
